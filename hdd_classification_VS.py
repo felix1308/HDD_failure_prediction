939ad682d39bdc928128d206f0930459c86abf71
felix1308 <95524545+felix1308@users.noreply.github.com>
2022-03-08 18:59:25 +0530

    Update hdd_classification_VS.py

--- hdd_classification_VS.py
+++ hdd_classification_VS.py
@@ -1,14 +1,7 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
 import pickle
 
 def HDD_prediction(x_test):
-    filename = "./HDD_classification_model.h5"
+    filename = "./HDD_classification_model.bin"
     model = pickle.load(open(filename, 'rb'))
 
     res = model.predict(x_test)
