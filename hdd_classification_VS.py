a637cc663d2b0d20b85aa3d2439cb864e28b3c82
felix1308 <95524545+felix1308@users.noreply.github.com>
2022-03-09 11:58:40 +0530

    Add files via upload

--- hdd_classification_VS.py
+++ hdd_classification_VS.py
@@ -1,7 +1,7 @@
 import pickle
 
 def HDD_prediction(x_test):
-    filename = "./HDD_classification_model.bin"
+    filename = "./HDD_classification_model.pkl"
     model = pickle.load(open(filename, 'rb'))
 
     res = model.predict(x_test)
